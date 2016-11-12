e8a819ec261a830341a790f638a143bdea56c70c
sidiarta <putuart6@gmail.com>
2016-11-12 05:53:42 -0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+# utsrekakayasaprotokol
+Kelompok 5,6 : Perintah yang dikirim client adalah “cpu” dengan balikan server adalah cpu usage pada server saat itu
+
+import socket
+
+s=  socket.socket()
+host =socket.gethostname()
+port = 12345
+s.connect((host,port))
+s.send('cpu')
+print s.recv(1024)
+s.close()
